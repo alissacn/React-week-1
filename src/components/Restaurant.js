c1356b09276ac1df27928c62fafccc8568b8aa0f
alissa <alissanishihara@gmail.com>
2022-05-12 20:46:04 +0200

    save files

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,31 +1,25 @@
-
 import React, { useState } from "react";
 import RestaurantButton from "../components/RestaurantButton";
 
-
 const Restaurant = () => {
-
   const [orders, setOrders] = useState(0);
 
-  function orderOne(props) {
+  function orderOne() {
     setOrders(orders + 1);
   }
 
-    return (
-      <div>
-        <h3>Restaurant Orders</h3>
-        <ul>
-            <li>
-                Pizzas:
-                <RestaurantButton add={orderOne} />
-                <button onClick={() => orderOne()} className="btn btn-primary">Add</button>
-            </li>
-        </ul>
-      </div>
-    );
-  };
+  return (
+    <div>
+      <h3>Restaurant Orders</h3>
+      <ul>
+        <li>
+          Pizzas:
+          {orders}
+          <RestaurantButton add={orderOne} />
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-
-
-
-  export default Restaurant;
+export default Restaurant;
