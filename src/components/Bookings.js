0002b1ddbb7b5520aed114e49a66f0a6bbbb4d54
alissa <alissanishihara@gmail.com>
2022-05-24 21:36:16 +0200

    finish exercises

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -4,27 +4,36 @@ import SearchResults from "../components/SearchResults";
 
 const Bookings = () => {
   const [bookings, setBookings] = useState([]);
+  const [isLoading, setIsloading] = useState(false);
 
   const handleSearch = searchTerm => {
     search(searchTerm);
   };
 
   useEffect(() => {
-    fetch("https://cyf-react.glitch.me/")
-      .then(res => res.json())
+    setIsloading(true);
+    fetch("https://cyf-react.glitch.me/delayed")
+      .then(res => {
+        setIsloading(false);
+        if (res.status != 200) {
+          alert("Sorry, something went wrong. Please try again.");
+        }
+        return res.json();
+      })
       .then(data => {
         setBookings(data);
       })
       .catch(err => {
-        alert("Sorry, something went wrong. Please try again.");
+        setIsloading(false);
+        console.log(err);
       });
   }, []);
 
   const search = searchVal => {
     const bookingsFiltered = bookings.filter(booking => {
       return (
-        booking.firstName[0].toUpperCase().includes(searchVal) ||
-        booking.surname[0].toUpperCase().includes(searchVal)
+        booking.firstName[0].toLowerCase().includes(searchVal.toLowerCase()) ||
+        booking.surname[0].toLowerCase().includes(searchVal.toUpperCase())
       );
     });
     console.log(bookingsFiltered);
@@ -35,6 +44,7 @@ const Bookings = () => {
     <div className="App-content">
       <div className="container">
         <Search search={search} onSearch={handleSearch} />
+        <div id="test">{isLoading ? "Loading..." : ""}</div>
         <SearchResults results={bookings} />
       </div>
     </div>
--- src/components/CustomerProfile.jsx
+++ src/components/CustomerProfile.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const CustomerProfile = id => {
-  return <div>{id ? `Customer profile: ${id}` : ""}</div>;
+const CustomerProfile = ({ id }) => {
+  const [customerInfo, setCustomerInfo] = useState(null);
+  useEffect(() => {
+    if (id > 0) {
+      fetch(`https://cyf-react.glitch.me/customers/${id}`)
+        .then(response => response.json())
+        .then(data => setCustomerInfo(data));
+    }
+  }, [id]);
+
+  return (
+    <div className="customer-profile text-center">
+      {id && customerInfo
+        ? ` Customer profile | ID: ${customerInfo.id} |
+    Email: ${customerInfo.email} |
+    Is VIP? ${customerInfo.vip} |
+    Phone Number: ${customerInfo.phoneNumber}`
+        : ""}
+    </div>
+  );
 };
 
 export default CustomerProfile;
